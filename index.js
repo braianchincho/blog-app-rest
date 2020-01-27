114a63eb0f9d2b939091b567675c2c06ab4cf390
braianchincho <braianchincho@gmail.com>
2020-01-27 03:28:30 -0300

    Configuracion de base de datos en la nube

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const iniciarRutas = require("./routes/index.routes");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const iniciarRutas = require('./routes/index.routes');
+const bodyParser = require('body-parser');
+const { userDB, passwordDB } = require('./settings.json');
+const url =`mongodb+srv://${userDB}:${passwordDB}@cluster0-ms4r4.azure.mongodb.net/test?retryWrites=true&w=majority`;
+const app = express();
+app.use(express.json()); // Make sure it comes back as json
 
-// Connect to MongoDB database
-mongoose
-  .connect("mongodb://localhost:27017/blogdb", { useNewUrlParser: true })
-  .then(() => {
-    iniciarAPI();
-  });
+mongoose.connect(url, { useNewUrlParser: true }, err => {
+  if (!err) {
+    console.log('Conectado a la bd en la nube');
+    iniciarAPI(); 
+  } else {
+    console.error('No se pudo conectar a la bd' + err)
+  }
+})
 
 function iniciarAPI() {
-  const app = express();
   app.use(bodyParser.json());
   iniciarRutas(app);
   
   app.listen(5000, () => {
-    console.log("Server has started!")
+    console.log('Server has started! on 5000 port')
   })
 }
 
