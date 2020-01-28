8d60a5756b162274032b2d4f8fc3309e2cc3c951
braianchincho <braianchincho@gmail.com>
2020-01-28 00:15:40 -0300

    add settings

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const iniciarRutas = require('./routes/index.routes');
 const bodyParser = require('body-parser');
-const { userDB, passwordDB } = require('./settings.json');
-const url =`mongodb+srv://${userDB}:${passwordDB}@cluster0-ms4r4.azure.mongodb.net/test?retryWrites=true&w=majority`;
-const app = express();
 const { handleError, } = require('./helpers/error');
 const httpLogger = require('./helpers/httpLogger');
+const { userDB, passwordDB } = require('./settings.json');
+
+const url =`mongodb+srv://${userDB}:${passwordDB}@cluster0-ms4r4.azure.mongodb.net/test?retryWrites=true&w=majority`;
+const app = express();
+
 app.use(express.json()); // Make sure it comes back as json
 
 mongoose.connect(url, { useNewUrlParser: true }, err => {
